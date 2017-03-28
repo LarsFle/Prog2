923199c390d6dd6f3a35a5534255de39d26e89c6
Larame <Larame@DESKTOP-PBNHHAT.fritz.box>
2017-03-28 20:57:10 +0200

    Entity class um Statusabfragen ergänzt

--- SquirrelGame/src/squirrelGame/Entity.java
+++ SquirrelGame/src/squirrelGame/Entity.java
@@ -4,4 +4,29 @@ public class Entity {
 	int id;
 	int energy;
 	XY pos;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public int energy() {
+		return energy;
+	}
+	public boolean updateEnergy(int amount) {
+		energy += amount;
+		if (energy >= 0) {
+			return true;
+		} else {
+			energy = 0;
+			return false;
+		}
+	}
+	public XY getXY() {
+		return pos;
+	}
+	public boolean setXY(XY newpos) {
+		pos = newpos;
+		return true;
+	}
+	
 }
