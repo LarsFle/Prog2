8be598ca39f8bbd793a00f2fefdd11580d0549bf
Larame <Larame@DESKTOP-PBNHHAT.fritz.box>
2017-04-02 17:08:27 +0200

    MasterSquirrel Steuerung eingebaut, hätte es gerne mit switchh gemacht, aber dann könnte man die Steuerung nicht variabel machen, da swtich cases constant sein müssen

--- SquirrelGame/src/squirrelGame/MasterSquirrel.java
+++ SquirrelGame/src/squirrelGame/MasterSquirrel.java
@@ -1,10 +1,11 @@
 package squirrelGame;
 
 public class MasterSquirrel extends Entity {
-	char LEFT_KEY;
-	char UP_KEY;
-	char DOWN_KEY;
-	char RIGHT_KEY;
+	final char LEFT_KEY;
+	final char UP_KEY;
+	final char DOWN_KEY;
+	final char RIGHT_KEY;
+	
 	public MasterSquirrel(int iid, XY ipos, char UPKEY, char LEFTKEY, char DOWNKEY, char RIGHTKEY) {
 		id = iid;
 		energy = 1000;
@@ -15,5 +16,21 @@ public class MasterSquirrel extends Entity {
 		RIGHT_KEY = RIGHTKEY;
 	}
 	
+	public boolean HandOperatedMasterSquirrel() throws Exception {
+		char c;
+		while (( c = (char)System.in.read()) != '\n') {
+			if (c == LEFT_KEY) {
+				pos.move(-1, 0);
+				} else if (c == UP_KEY) {
+				pos.move(0, -1);;
+				} else if (c == DOWN_KEY) {
+				pos.move(0, 1);
+				} else if (c == RIGHT_KEY) {
+				pos.move(1, 0);
+				}
+			}
+		}
+		return true;
+	}
 	
 }
