d053cb40d8e751270ba965179c1e202b420c00c4
Larame <Larame@DESKTOP-PBNHHAT.fritz.box>
2017-04-02 16:16:35 +0200

    Variable Tastenbelegung fürs Eichhörnchen (4 Richtungen) eingebaut

--- SquirrelGame/src/squirrelGame/MasterSquirrel.java
+++ SquirrelGame/src/squirrelGame/MasterSquirrel.java
@@ -1,9 +1,19 @@
 package squirrelGame;
 
 public class MasterSquirrel extends Entity {
-	public MasterSquirrel(int iid, XY ipos) {
+	char LEFT_KEY;
+	char UP_KEY;
+	char DOWN_KEY;
+	char RIGHT_KEY;
+	public MasterSquirrel(int iid, XY ipos, char UPKEY, char LEFTKEY, char DOWNKEY, char RIGHTKEY) {
 		id = iid;
 		energy = 1000;
 		pos = ipos;
+		LEFT_KEY = LEFTKEY;
+		UP_KEY = UPKEY;
+		DOWN_KEY = DOWNKEY;
+		RIGHT_KEY = RIGHTKEY;
 	}
+	
+	
 }
